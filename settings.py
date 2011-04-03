9eb500f068f5352e48f68bda6f798e21d9c6b5a3
Peppe Bergqvist <p@bergqvi.st>
2011-04-03 07:10:46 +0200

    random

--- settings.py
+++ settings.py
@@ -1,37 +1,7 @@
 import os
 
-
-#########################################################################
-# Import settings from local_settings.py, if it exists.
-#
-# Put this at the end of settings.py
-
-# try:
-#   import local_settings
-# except ImportError:
-#   print """ 
-#     -------------------------------------------------------------------------
-#     You need to create a local_settings.py file which needs to contain at least
-#     database connection information.
-    
-#     Copy local_settings_example.py to local_settings.py and edit it.
-#     -------------------------------------------------------------------------
-#     """
-#   import sys 
-#   sys.exit(1)
-  
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'babsbasbdsadjaksdbnjasd'
-
-ADMINS = (
-    ('Peppe Bergqvist', 'p@bergqvi.st'),
-)
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
-MANAGERS = ADMINS
 
 TIME_ZONE = 'Europe/Stockholm'
 
@@ -76,7 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.syndication',
     
     'articles',
-    'south',
+    # 'south',
     # 'sitetree',
     'mptt',
     'treenav',
@@ -89,3 +59,40 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.media',
 )
+
+
+#########################################################################
+# Import settings from local_settings.py, if it exists.
+#
+# Put this at the end of settings.py
+
+try:
+    import local_settings
+except ImportError:
+    print """ 
+        -------------------------------------------------------------------------
+        You need to create a local_settings.py file which needs to contain at least
+        database connection information.
+
+        Copy local_settings_example.py to local_settings.py and edit it.
+        -------------------------------------------------------------------------
+        """
+    import sys 
+    sys.exit(1)
+else:
+    # Import any symbols that begin with A-Z. Append to lists any symbols that
+    # begin with "EXTRA_".
+    import re
+    for attr in dir(local_settings):
+        # print attr
+        match = re.search('^EXTRA_(\w+)', attr)
+        if match:
+            # print "match"
+            name = match.group(1)
+            value = getattr(local_settings, attr)
+            try:
+                globals()[name] += value
+            except KeyError:
+                globals()[name] = value
+        elif re.search('^[A-Z]', attr):
+            globals()[attr] = getattr(local_settings, attr)  

3ff5fea6a6085250bc622cb9403eaa32fefb1352
Peppe Bergqvist <p@bergqvi.st>
2012-05-16 18:14:37 +0200

    test of sftp

--- pipiki/storage.py
+++ pipiki/storage.py
@@ -35,7 +35,7 @@ class CachedSFTPStorage(SFTPStorage):
         return name
 
     def path(self, name):
-#        return None
+        return None
         print self.local_storage.path(name)
         return self.local_storage.path(name)
 

f5a6b7c7c4860fb8c3c2adeb57dd3f0d5e678a45
Peppe Bergqvist <p@bergqvi.st>
2012-05-16 14:40:43 +0200

    test of s3

--- pipiki/storage.py
+++ pipiki/storage.py
@@ -58,5 +58,5 @@ class CachedS3BotoStorage(S3BotoStorage):
         return name
 
     def path(self, name):
-        return None
-#        return self.local_storage.path(name)
+#        return None
+        return self.local_storage.path(name)

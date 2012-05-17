dcb28a0e7f46187e10af51e8be0ae3469b2c4d0d
Peppe Bergqvist <p@bergqvi.st>
2012-05-17 13:26:17 +0200

    added known_hosts file

--- flatpages/views.py
+++ flatpages/views.py
@@ -31,6 +31,8 @@ def edit_hosts_file(request):
 
     if not os.path.exists(os.path.join(settings.MEDIA_ROOT, 'ssh')):
         os.makedirs(os.path.join(settings.MEDIA_ROOT, 'ssh'))
+        
+    if not os.path.isfile(settings.SFTP_KNOWN_HOST_FILE):
         f = file(settings.SFTP_KNOWN_HOST_FILE, "w")
         f.close()
 

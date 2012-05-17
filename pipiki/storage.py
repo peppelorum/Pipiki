64a0affa6e9bdad311a528e6ca3b26535b04f4b2
Peppe Bergqvist <p@bergqvi.st>
2012-05-17 20:51:27 +0200

    Removed ftpstorage

--- pipiki/storage.py
+++ pipiki/storage.py
@@ -5,22 +5,22 @@ import os
 import paramiko
 
 from django.core.files.storage import get_storage_class
-from storages.backends.ftp import FTPStorage
+#from storages.backends.ftp import FTPStorage
 from storages.backends.sftpstorage import SFTPStorage
 
-class CachedFTPStorage(FTPStorage):
-    """
-    FTP storage backend that saves the files locally, too.
-    """
-    def __init__(self, *args, **kwargs):
-        super(CachedFTPStorage, self).__init__(*args, **kwargs)
-        self.local_storage = get_storage_class(
-            "compressor.storage.CompressorFileStorage")()
-
-    def save(self, name, content):
-        name = super(CachedFTPStorage, self).save(name, content)
-        self.local_storage._save(name, content)
-        return name
+#class CachedFTPStorage(FTPStorage):
+#    """
+#    FTP storage backend that saves the files locally, too.
+#    """
+#    def __init__(self, *args, **kwargs):
+#        super(CachedFTPStorage, self).__init__(*args, **kwargs)
+#        self.local_storage = get_storage_class(
+#            "compressor.storage.CompressorFileStorage")()
+#
+#    def save(self, name, content):
+#        name = super(CachedFTPStorage, self).save(name, content)
+#        self.local_storage._save(name, content)
+#        return name
 
 
 class CachedSFTPStorage(SFTPStorage):

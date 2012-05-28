9ca7a135c8371f3fa949f3b5b44b4c05496c405a
Peppe Bergqvist <p@bergqvi.st>
2012-05-28 19:23:58 +0200

    use another db than Gondor

--- deploy/settings_primary.py
+++ deploy/settings_primary.py
@@ -1,4 +1,6 @@
 
+import os
+
 #DEBUG = True
 
 STATIC_URL = 'http://cdn.bergqvi.st/pipiki/static/'
@@ -6,3 +8,15 @@ STATIC_URL = 'http://cdn.bergqvi.st/pipiki/static/'
 MEDIA_URL = 'http://cdn.bergqvi.st/pipiki/static/'
 
 SITE_ID = 1
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTUSER'),
+        'HOST': os.getenv('POSTHOST'),
+        'USER': os.getenv('POSTUSER'),
+        #    'PORT': 33060,
+        'PASSWORD': os.getenv('POSTPASSWORD'),
+        }
+}
